0580e0e0913c70d3ef223d29db2b5dba601d15c0
Dominik <dono3108@gmail.com>
2023-03-08 23:34:31 +0100

    init commit

---
+++ src/main/java/com/domanski/juniorofferproject/JuniorofferprojectApplication.java
@@ -0,0 +1,13 @@
+package com.domanski.juniorofferproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorofferprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorofferprojectApplication.class, args);
+	}
+
+}
