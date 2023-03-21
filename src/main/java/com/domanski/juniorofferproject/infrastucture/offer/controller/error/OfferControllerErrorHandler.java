1bb2da19c4d21fd91f8d77c83093644a17cf4b86
Dominik <dono3108@gmail.com>
2023-03-21 20:27:08 +0100

    add duplicate key exception handler

--- src/main/java/com/domanski/juniorofferproject/infrastucture/offer/controller/error/OfferControllerErrorHandler.java
+++ src/main/java/com/domanski/juniorofferproject/infrastucture/offer/controller/error/OfferControllerErrorHandler.java
@@ -2,6 +2,7 @@ package com.domanski.juniorofferproject.infrastucture.offer.controller.error;
 
 import com.domanski.juniorofferproject.domain.offer.OfferNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,5 +20,14 @@ public class OfferControllerErrorHandler {
         String message = exception.getMessage();
         log.error(message);
         return new OfferErrorResponse(message, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public OfferPostErrorResponse handleDuplicateKeyException() {
+        log.error("Try save offer with url what already exist in database");
+        String message = "Given offer is already exist";
+        return new OfferPostErrorResponse(message, HttpStatus.CONFLICT);
     }
 }
